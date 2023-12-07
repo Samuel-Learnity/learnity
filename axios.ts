1bc19c243444c01dfee55b4b69239c31d4fd2b99
Samuel-Learnity <samuel@learnity.app>
2023-12-07 14:57:02 +0100

    key ngrok

--- axios.ts
+++ axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://c340-2001-861-3dca-750-74d7-466f-8d13-5409.ngrok-free.app',
+    baseURL: 'https://0623-2001-861-3dca-750-98d0-95a2-d874-7598.ngrok-free.app',
 });
